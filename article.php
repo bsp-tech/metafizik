7fba7e21d36040c270ed20a05d9b837bbff14c0a
Sarkhan Rasullu <bsptechou@gmail.com>
2019-09-25 23:25:00 +0200

    changes

--- article.php
+++ article.php
@@ -8,20 +8,20 @@
 <body>
 	<?php require('header.php');?>
 	<div class="container ">
-		<div class="row mt-5" >
-			<div class="col-sm-12 col-md-12 col-lg-5" >
-					<img style="width: 100%" src="https://scontent.fgyd8-1.fna.fbcdn.net/v/t1.0-9/58574677_2418358381548055_4722803698617548800_n.jpg?_nc_cat=109&_nc_oc=AQnfh2IjxzlqaS4YMeqdk7T-JtYni8R6BBQzFjBz3_Xs01Cv8tj49GIIWXEqH_kCl70&_nc_ht=scontent.fgyd8-1.fna&oh=aba34b69423ca84fe01ca6e1fa0d45bd&oe=5E01B0CF">
+		<div class="row mt-5">
+			<div class="col-sm-12 col-md-12 text-center mb-5">
+					<img src="https://scontent.fgyd8-1.fna.fbcdn.net/v/t1.0-9/58574677_2418358381548055_4722803698617548800_n.jpg?_nc_cat=109&amp;_nc_oc=AQnfh2IjxzlqaS4YMeqdk7T-JtYni8R6BBQzFjBz3_Xs01Cv8tj49GIIWXEqH_kCl70&amp;_nc_ht=scontent.fgyd8-1.fna&amp;oh=aba34b69423ca84fe01ca6e1fa0d45bd&amp;oe=5E01B0CF" class="w-50">
 			</div>
-			<div class="col-sm-12 col-md-12 col-lg-7" >
-				<h4 >SELAHATTIN AKKAYA KIMDIR?</h4>
+			<div class="col-sm-12 col-md-12">
+				<h4>SELAHATTIN AKKAYA KIMDIR?</h4>
 				<p class="text-muted">
-					Selahattin AKKAYA 1966 Erzurum doğumludur. Eğitim ve Öğretimini İstanbul’da tamamlamıştır. Dünya çapında bu hizmeti veren AKKAYA gerekli bilgi, birikim, donanım ve referansa sahiptir.<br/>
+					Selahattin AKKAYA 1966 Erzurum doğumludur. Eğitim ve Öğretimini İstanbul’da tamamlamıştır. Dünya çapında bu hizmeti veren AKKAYA gerekli bilgi, birikim, donanım ve referansa sahiptir.<br>
 
-					Metafizik, bioenerji ve frekanslarla tedavi yöntemleri alanında uzmandır.<br/>
+					Metafizik, bioenerji ve frekanslarla tedavi yöntemleri alanında uzmandır.<br>
 
-					Akkaya hastalarının teşhis ve tedavi sürecinde bütün ihtimallere önem vererek bilimin ışığında bu süreçleri yönetmektedir. Akkaya, uzman olduğu alanlarda hem yurtiçi hem yurtdışında yapmış olduğu çalışmalar neticesinde ismini dünyaya duyurmuştur.</br>
+					Akkaya hastalarının teşhis ve tedavi sürecinde bütün ihtimallere önem vererek bilimin ışığında bu süreçleri yönetmektedir. Akkaya, uzman olduğu alanlarda hem yurtiçi hem yurtdışında yapmış olduğu çalışmalar neticesinde ismini dünyaya duyurmuştur.<br>
 
-					Türkiye’de bu alanın gelişmesi ve ülkeye pozitif anlamda katkı sağlaması için profesyonel çalışmalara devam eden Akkaya, yerli ve milli önem kaygısı ile yurtiçinde çeşitli eğitimler vermektedir.</br>
+					Türkiye’de bu alanın gelişmesi ve ülkeye pozitif anlamda katkı sağlaması için profesyonel çalışmalara devam eden Akkaya, yerli ve milli önem kaygısı ile yurtiçinde çeşitli eğitimler vermektedir.<br>
 				</p>
 			</div>
 		</div>		
--- articles.php
+++ articles.php
@@ -21,10 +21,10 @@
 
                     <div class="row my-3">
                         <div class="col-4">
-                             <a href="№" class = "text-decoration-none text-dark"><img src="imgArticle/1.jpg" alt="" class = "w-100"></a>
+                             <a href="/article.php" class = "text-decoration-none text-dark"><img src="imgArticle/1.jpg" alt="" class = "w-100"></a>
                         </div>
                         <div class="col-8">
-                            <h5><a href="№" class = "text-decoration-none text-dark">Kötümserliğin Metafizik Nedeni</a></h5>
+                            <h5><a href="/article.php" class = "text-decoration-none text-dark">Kötümserliğin Metafizik Nedeni</a></h5>
                             <p class = "text-muted pb-0 mb-0"> 
                                     Bu noktadan başlayarak Schopenhauer’ın kötümserliğinin nedenini daha iyi anlamış olacağız.
                             </p>
@@ -36,10 +36,10 @@
 
                     <div class="row my-3">
                         <div class="col-4">
-                             <a href="№" class = "text-decoration-none text-dark"><img src="imgArticle/2.jpg" alt="" class = "w-100"></a>
+                             <a href="/article.php" class = "text-decoration-none text-dark"><img src="imgArticle/2.jpg" alt="" class = "w-100"></a>
                         </div>
                         <div class="col-8">
-                            <h5><a href="№" class = "text-decoration-none text-dark">İlk İlkelerin Bilimi Olarak Metafizik</a></h5>
+                            <h5><a href="/article.php" class = "text-decoration-none text-dark">İlk İlkelerin Bilimi Olarak Metafizik</a></h5>
                             <p class = "text-muted pb-0 mb-0"> 
                                     Metafiziğin üçüncü bir alanı ilk ilkelere ilişkin soruşturmalardan oluşur veya metafiziksel problemlerin üçüncü bir öbeği, doğayla ilgili araştırmalardan kaynaklanan problemlerin oluşturduğu öbektir.
                             </p>
@@ -47,14 +47,12 @@
                             <p class = "blockquote-footer text-right py-0 my-0"><i class="far fa-clock"></i>  19 Eylül 2018 Çarşamba</p>
                         </div>
                     </div>
-
-
                     <div class="row my-3">
                         <div class="col-4">
-                             <a href="№" class = "text-decoration-none text-dark"><img src="imgArticle/3.jpg" alt="" class = "w-100"></a>
+                             <a href="/article.php" class = "text-decoration-none text-dark"><img src="imgArticle/3.jpg" alt="" class = "w-100"></a>
                         </div>
                         <div class="col-8">
-                            <h5><a href="№" class = "text-decoration-none text-dark">Teolojik ve Kozmolojik Metafizik</a></h5>
+                            <h5><a href="/article.php" class = "text-decoration-none text-dark">Teolojik ve Kozmolojik Metafizik</a></h5>
                             <p class = "text-muted pb-0 mb-0">   
                                     Metafiziğin ikinci ana bölümü, bu kez ontolojiyle değil de sırasıyla teoloji ve kozmolojiyle özdeşleşen bir disiplin ya da araştırma türü olarak metafiziktir.
                             </p>
@@ -62,32 +60,25 @@
                             <p class = "blockquote-footer text-right py-0 my-0"><i class="far fa-clock"></i>  19 Eylül 2018 Çarşamba</p>
                         </div>
                     </div>
-
-                
-
                     <div class="row my-3">
                         <div class="col-4">
-                             <a href="№" class = "text-decoration-none text-dark"><img src="imgArticle/4.jpg" alt="" class = "w-100"></a>
+                             <a href="/article.php" class = "text-decoration-none text-dark"><img src="imgArticle/4.jpg" alt="" class = "w-100"></a>
                         </div>
                         <div class="col-8">
-                            <h5><a href="№" class = "text-decoration-none text-dark">Varoluş Felsefesi veya Varoluşculuk</a></h5>
+                            <h5><a href="/article.php" class = "text-decoration-none text-dark">Varoluş Felsefesi veya Varoluşculuk</a></h5>
                             <p class = "text-muted pb-0 mb-0"> 
                                     Bu varlık görüşünde insan tanımı, varlıktan değil, bizzat insandan çıkılarak yapılır ve çok daha önemlisi, varlık “kendi kendisini tanımlayan insan”a göre tanımlanan bir şey olarak görülür. Başka bir deyişle, bu yeni felsefede varlık, varlığı ele alan, varlık sorusunu sorabilen yegâne varlık olarak insandan hareketle ortaya konur.
-
                             </p>
                             <br>
                             <p class = "blockquote-footer text-right py-0 my-0"><i class="far fa-clock"></i>  19 Eylül 2018 Çarşamba</p>
                         </div>
                     </div>
-
-                
-
                     <div class="row my-3">
                         <div class="col-4">
-                            <a href="№" class = "text-decoration-none text-dark"><img src="imgArticle/5.jpg" alt="" class = "w-100"></a>
+                            <a href="/article.php" class = "text-decoration-none text-dark"><img src="imgArticle/5.jpg" alt="" class = "w-100"></a>
                         </div>
                         <div class="col-8">
-                            <h5><a href="№" class = "text-decoration-none text-dark">Süreç ya da Oluş Felsefesi</a></h5>
+                            <h5><a href="/article.php" class = "text-decoration-none text-dark">Süreç ya da Oluş Felsefesi</a></h5>
                             <p class = "text-muted pb-0 mb-0"> 
                                     Süreç ya da oluş felsefesi doğanın sürekli olarak değişen olay dizilerinden meydana geldiğini, gerçekliğin temelinde, tözün değil de sürecin, yani belli bir doğrultusu olan bir değişmenin bulunduğunu öne sürer.
                             </p>
@@ -95,15 +86,12 @@
                             <p class = "blockquote-footer text-right py-0 my-0"><i class="far fa-clock"></i>  19 Eylül 2018 Çarşamba</p>
                         </div>
                     </div>
-
-                
-
                     <div class="row my-3">
                         <div class="col-4">
-                            <a href="№" class = "text-decoration-none text-dark"><img src="imgArticle/6.jpg" alt="" class = "w-100"></a>
+                            <a href="/article.php" class = "text-decoration-none text-dark"><img src="imgArticle/6.jpg" alt="" class = "w-100"></a>
                         </div>
                         <div class="col-8">
-                            <h5><a href="№" class = "text-decoration-none text-dark">Metafizik Nedir?</a></h5>
+                            <h5><a href="/article.php" class = "text-decoration-none text-dark">Metafizik Nedir?</a></h5>
                             <p class = "text-muted pb-0 mb-0"> 
                                     Metafizik, felsefenin belirli bir bölümüdür. Bu bölümde, “Varlık nedir?”, “Bir dış dünya var mıdır?”, “Beden ile ruh arasındaki ilişkiler nelerdir?”, “Tanrı var mıdır?”, “Ruh ölümlü müdür, ölümsüz müdür?“ gibi sorulara cevaplar aranır. Ayrıca, bilginin nereden geldiği, neleri bilebileceğimiz de araştırılır. Ama metafizikteki asıl araştırma konuları, evren, tanrı ve ruhtur.
 
@@ -112,15 +100,12 @@
                             <p class = "blockquote-footer text-right py-0 my-0"><i class="far fa-clock"></i>  19 Eylül 2018 Çarşamba</p>
                         </div>
                     </div>
-
-                
-
                     <div class="row my-3">
                         <div class="col-4">
-                            <a href="№" class = "text-decoration-none text-dark"><img src="imgArticle/7.jpg" alt="" class = "w-100"></a>
+                            <a href="/article.php" class = "text-decoration-none text-dark"><img src="imgArticle/7.jpg" alt="" class = "w-100"></a>
                         </div>
                         <div class="col-8">
-                            <h5><a href="№" class = "text-decoration-none text-dark">Modern Töz Metafiziği</a></h5>
+                            <h5><a href="/article.php" class = "text-decoration-none text-dark">Modern Töz Metafiziği</a></h5>
                             <p class = "text-muted pb-0 mb-0"> 
                                     Modern töz metafiziği, on yedinci yüzyıldan itibaren ya Kıta Avrupa’sında Descartes ya da Ada Avrupa’sında Hobbes ve Locke benzeri filozoflar tarafından geliştirilmiştir. Söz konusu metafizik, ya tek tözlü ya da iki tözlü bir varlık anlayışına dayandığı için, ondan dört farklı alternatif veya metafizik öğreti çıkar.
                             </p>
@@ -128,15 +113,12 @@
                             <p class = "blockquote-footer text-right py-0 my-0"><i class="far fa-clock"></i>  19 Eylül 2018 Çarşamba</p>
                         </div>
                     </div>
-
-                
-
                     <div class="row my-3">
                         <div class="col-4">
-                            <a href="№" class = "text-decoration-none text-dark"><img src="imgArticle/8.jpg" alt="" class = "w-100"></a>
+                            <a href="/article.php" class = "text-decoration-none text-dark"><img src="imgArticle/8.jpg" alt="" class = "w-100"></a>
                         </div>
                         <div class="col-8">
-                            <h5><a href="№" class = "text-decoration-none text-dark">İlk İlkelerin Bilimi Olarak Metafizik</a></h5>
+                            <h5><a href="/article.php" class = "text-decoration-none text-dark">İlk İlkelerin Bilimi Olarak Metafizik</a></h5>
                             <p class = "text-muted pb-0 mb-0"> 
                                     Metafiziğin üçüncü bir alanı ilk ilkelere ilişkin soruşturmalardan oluşur veya metafiziksel problemlerin üçüncü bir öbeği, doğayla ilgili araştırmalardan kaynaklanan problemlerin oluşturduğu öbektir.
                             </p>
